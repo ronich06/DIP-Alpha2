bf6d3b7436d9c26e3576aa751d1eacc202fe257f
ronich06 <48327426+ronich06@users.noreply.github.com>
2021-03-20 16:15:44 -0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Ejemplo_DIP2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejemplo_DIP2.dir/main.cpp.obj"
+  "Ejemplo_DIP2.exe"
+  "Ejemplo_DIP2.exe.manifest"
+  "Ejemplo_DIP2.pdb"
+  "libEjemplo_DIP2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejemplo_DIP2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
